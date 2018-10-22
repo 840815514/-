998c8e36b9f9d55ff768a58d3d19b49c7e063116
尤艺琪 <youyiqi@youyiqidemacbook-pro.local>
2018-10-22 09:37:45 +0800

    Initial Commit

---
+++ 悬停/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  悬停
+//
+//  Created by 尤艺琪 on 2018/10/22.
+//  Copyright © 2018年 尤艺琪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 悬停/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  悬停
+//
+//  Created by 尤艺琪 on 2018/10/22.
+//  Copyright © 2018年 尤艺琪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
